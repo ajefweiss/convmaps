4e7124e48ff7e68b963ea4316b0f6b66cbee73b8
Andreas Jeffery Weiss <ajefweiss@gmail.com>
2019-02-18 11:42:32 +0100

    Added util/maps.py

---
+++ convmaps/util/maps.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""maps.py
+
+"""
+
+import numpy as np
+
+
+def ang_dist(theta, phi, theta2=0, phi2=0):
+    """
+    Calculates angular distance between to points (RA/DEC system)
+
+    Args:
+        theta: target declination
+        phi: target right-ascension
+        theta2: base declination
+        phi2: base right-ascension
+
+    Returns: angle (radians)
+
+    """
+    if isinstance(theta, np.ndarray) and isinstance(phi, np.ndarray):
+        if isinstance(float(theta2), float):
+            theta2 = theta2 * np.ones_like(theta)
+
+        if isinstance(float(phi2), float):
+            phi2 = phi2 * np.ones_like(phi)
+
+    return np.arccos(np.sin(phi) * np.sin(phi2) + np.cos(phi) * np.cos(phi2) * np.cos(theta - theta2))
